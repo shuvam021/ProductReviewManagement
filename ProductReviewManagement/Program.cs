b8ff376a00e24cd04a1cc5567654b8a7d4744c97
git-kronos <repository.shuvam@gmail.com>
2022-02-19 21:53:17 +0530

    initial commit

---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    class Program
+    {
+        static void ProductReviewMethods()
+        {
+            var products = new List<Product>()
+            {
+                new Product() {Id = 1, UserId = 1, Review = "good", Rating = 17, IsLike = true},
+                new Product() {Id = 1, UserId = 1, Review = "good", Rating = 17, IsLike = true},
+                new Product() {Id = 1, UserId = 1, Review = "good", Rating = 17, IsLike = true},
+                new Product() {Id = 1, UserId = 1, Review = "good", Rating = 17, IsLike = true},
+                new Product() {Id = 1, UserId = 1, Review = "good", Rating = 17, IsLike = true},
+            };
+            var app = new ProductReviewController();
+            app.Create(products);
+            app.View();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Product Review Management!!!");
+            DataTableClass.View(DataTableClass.Create());
+        }
+    }
+}
